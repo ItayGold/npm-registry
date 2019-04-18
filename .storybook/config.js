6170e36637d3014e5ae79b25c688a2bc4c654a24
ItayGold <itil.gold@gmail.com>
2019-04-18 22:55:56 +0300

    storybook implementation

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,10 @@
 import { configure } from '@storybook/angular';
-// import { withInfo } from '@storybook/addon-info';
+import { withInfo } from '@storybook/addon-info';
 
 function loadStories() {
   require('../stories/index.js');
   // You can require as many stories as you need.
 }
 
-// addDecorator(withInfo);
+addDecorator(withInfo);
 configure(loadStories, module);
--- stories/index.js
+++ stories/index.js
@@ -1,36 +1,41 @@
 import { storiesOf } from '@storybook/angular';
+// import { withInfo } from '@storybook/addon-info';
+// import { withNotes } from '@storybook/addon-notes';
 // import { Button } from '@storybook/angular/demo';
 import { DatePickerComponent } from '@click/date-picker';
 
 storiesOf('@click date picker', module)
-  .addDecorator(
-    moduleMetadata({
-      declarations: [DatePickerComponent],
-    })
-  )
-  .add('with text', () => ({
-    component: DatePickerComponent,
-    props: {
-      setDateEvent: $event => {
-        const d = new Date($event);
-        const year = d.getFullYear();
-        let month = '' + (d.getMonth() + 1);
-        let day = '' + d.getDate();
+  // .addDecorator(
+  //   moduleMetadata({
+  //     declarations: [DatePickerComponent],
+  //   })
+  // )
+  // .addDecorator(withNotes)
+  .add(
+    'with text',
+    () => ({
+      component: DatePickerComponent,
+      props: {
+        setDateEvent: $event => {
+          const d = new Date($event);
+          const year = d.getFullYear();
+          let month = '' + (d.getMonth() + 1);
+          let day = '' + d.getDate();
 
-        if (month.length < 2) {
-          month = '0' + month;
-        }
-        if (day.length < 2) {
-          day = '0' + day;
-        }
+          if (month.length < 2) {
+            month = '0' + month;
+          }
+          if (day.length < 2) {
+            day = '0' + day;
+          }
 
-        return [year, month, day].join('-');
+          return [year, month, day].join('-');
+        },
+        selectedDate: new Date(),
+        todayDate: new Date(),
+        todayButtonText: `Today`,
       },
-      selectedDate: new Date(),
-      todayDate: new Date(),
-      todayButtonText: `Today`,
-    },
-    template: `
+      template: `
     <span
     ><i
       role="button"
@@ -51,4 +56,7 @@ storiesOf('@click date picker', module)
   >
   </click-date-picker>
   `,
-  }));
+    })
+    // { info: { inline: true, header: false } },
+    // { notes: 'My notes on some button' }
+  );
