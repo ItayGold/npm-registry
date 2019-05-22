b88b9a761cc648a32152d4313cfa8e08d6ad54c3
ItayGold <itil.gold@gmail.com>
2019-05-22 23:55:24 +0300

    cli pack -p complete

--- cli.js
+++ cli.js
@@ -1,28 +1,36 @@
+#! /usr/bin/env node --harmony
+
 const program = require('commander');
 const colors = require('colors');
+const version = require('./package.json').version;
 
 program
-  .version('1.0.0', '-v, --version')
+  .version(version, '-v, --version')
   .usage('[options] <file ...>')
-  .option('-p --package <package>', 'Package Name');
+  .command('pack')
+  .description('run pack command for required package')
+  .option('-p --package <package>', 'Package Name')
+  .action(({ package } = { ...options }) => {
+    if (typeof package === 'undefined') {
+      console.error(`Please run the script with the package name`.yellow.bold);
+      process.exit(1);
+    }
 
-program
-  .command('setup [env]')
-  .description('run setup commands for all envs')
-  .option('-s, --setup_mode [mode]', 'Which setup mode to use')
-  .action(function(env, options) {
-    var mode = options.setup_mode || 'normal';
-    env = env || 'all';
-    console.log(`setup for ${env} env(s) with ${mode} mode`);
+    console.log(
+      colors.green.underline('The selected package is:'),
+      `${package}`
+    );
   });
 
+program.on('command:*', function() {
+  console.log(
+    '\nInvalid command: '.red.bold,
+    program.args.join(' ').bold,
+    '\n\nSee a list of available commands below.\n'.magenta.underline.bold
+  );
+
+  program.help();
+});
+
 program.parse(process.argv);
-
-if (typeof program.package === 'undefined') {
-  console.error(`Please run the script with the package name`.yellow);
-  process.exit(1);
-}
-
-console.log(`The selected package is: ${program.package}`);
-
-
+if (program.args.length === 0) program.help();
