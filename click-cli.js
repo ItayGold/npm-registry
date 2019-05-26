cd41ef09ca26bfa8f0d2cfa64a51b6c649f06fe1
ItayGold <itil.gold@gmail.com>
2019-05-26 10:10:47 +0300

    click cli conitnue...

--- click-cli.js
+++ click-cli.js
@@ -13,59 +13,62 @@ const STATUS_MSG = {
 const STYLE_MSG = {
   MAGENTA: 'magenta',
   GREEN: 'green',
+  YELLOW: 'yellow',
 };
 
 function log(msg, { status, isUpperCase, color }) {
   const COLOR =
-    STATUS_MSG[status] === status ? STYLE_MSG.MAGENTA : STYLE_MSG.GREEN;
+    STATUS_MSG.START === status ? STYLE_MSG.MAGENTA : STYLE_MSG.GREEN;
   const message = isUpperCase ? msg.toUpperCase() : msg;
-
-  console.log(colors[color || COLOR]`${message}`);
+  return console.log(colors[color || COLOR](`${message}`));
 }
 
 program.version(pkgJson.version, '-v, --version').usage('[command][options]');
 
 program
-  .command('pack')
+  .command('publish')
+  .alias('pub')
   .description(
     'Pack and publish your required package. Sure the package name is correct.'
   )
   .option('-p --package <package>', 'Package Name')
   .action(({ package } = { ...options }) => {
     if (typeof package === 'undefined') {
-      console.error(`Please run the script with the package name`.yellow.bold);
+      log(`Please run the script with the package name`, {
+        status: null,
+        isUpperCase: false,
+        color: 'yellow',
+      });
       process.exit(1);
     }
 
-    log(`Start click pack process...`, {
+    log(`Start click publish process...`, {
       status: 'start',
       isUpperCase: true,
       color: 'blue',
     });
 
-    log(`Removing the ${package} package in the distrubution folder...`, {
-      status: 'start',
+    execSync(`rm -rf dist/${package}`, { stdio: 'inherit' });
+
+    log(`The dist ${package} has been removed in the distrubution folder.`, {
+      status: 'end',
       isUpperCase: true,
     });
 
-    console.log(
-      `Removing the ${package} package in the distrubution folder...`.toUpperCase()
-        .magenta
-    );
+    execSync(`ng build ${package}`, { stdio: 'inherit' });
 
-    execSync(`rm -rf dist/${package}`, { stdio: 'inherit' });
-    console.log(
-      `The dist package has been removed in the distrubution folder.`.toUpperCase()
-        .green
-    );
+    log(`The build of ${package} has been completed.`, {
+      status: 'end',
+      isUpperCase: true,
+    });
 
-    execSync(`npm run build ${package} --verbose`, { stdio: 'inherit' });
     execSync(`cd projects/${package} && npm version patch`, {
       stdio: 'inherit',
     });
-    execSync(`ng build ${package} --verbose`, { stdio: 'inherit' });
-    execSync(`cd dist/${package} && npm pack --verbose`, { stdio: 'inherit' });
-    execSync(`cd dist/${package} && npm publish --verbose`, {
+
+    execSync(`cd dist/${package} && npm pack`, { stdio: 'inherit' });
+
+    execSync(`cd dist/${package} && npm publish`, {
       stdio: 'inherit',
     });
   });
