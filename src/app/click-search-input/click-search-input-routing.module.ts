f387a3a765b1a5136bf4b28294aee447c31c3372
Denis.Demidenko <Denis.Demidenko@clicksoftware.com>
2019-07-08 06:52:30 -0700

    add search input demo component

--- src/app/click-search-input/click-search-input-routing.module.ts
+++ src/app/click-search-input/click-search-input-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ClickSearchInputComponent } from './click-search-input/click-search-input.component';
+import { ClickSearchInputDemoComponent } from './click-search-input-demo/click-search-input-demo.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: ClickSearchInputComponent,
+    component: ClickSearchInputDemoComponent,
   },
 ];
 
--- src/app/click-search-input/click-search-input.module.ts
+++ src/app/click-search-input/click-search-input.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClickSearchInputRoutingModule } from './click-search-input-routing.module';
 import { ClickSearchInputComponent } from './click-search-input/click-search-input.component';
+import { ClickSearchInputDemoComponent } from './click-search-input-demo/click-search-input-demo.component';
 
 @NgModule({
   declarations: [
-    ClickSearchInputComponent
+    ClickSearchInputComponent,
+    ClickSearchInputDemoComponent,
   ],
   imports: [
     CommonModule,
