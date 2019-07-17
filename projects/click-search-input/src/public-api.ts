099576c3fcf25987b5f5ac5e2a439849a87c081c
Denis.Demidenko <Denis.Demidenko@clicksoftware.com>
2019-07-17 08:14:46 -0700

    remove export unused service

--- projects/click-search-input/src/public-api.ts
+++ projects/click-search-input/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of click-search-input
  */
 
-export * from './lib/click-search-input.service';
 export * from './lib/click-search-input.component';
 export * from './lib/click-search-input.module';
