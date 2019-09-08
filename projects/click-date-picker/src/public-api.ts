48a56edd212d45594456f3e247b6758403dba295
ItayGold <itil.gold@gmail.com>
2019-09-08 18:24:29 +0300

    129274:Not compatible build of diff Angular versions

--- projects/click-date-picker/src/public-api.ts
+++ projects/click-date-picker/src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of click-date-picker
  */
 
-export * from './lib/click-date-picker.service';
 export * from './lib/click-date-picker/click-date-picker.component';
 export * from './lib/click-date-picker-range/click-date-picker-range.component';
 export * from './lib/click-date-picker.module';
