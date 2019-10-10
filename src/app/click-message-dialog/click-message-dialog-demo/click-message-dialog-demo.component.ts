a9a7d5336e8ed6df501f7fb23dade3e8134d3b62
ItayGold <itil.gold@gmail.com>
2019-10-10 11:40:43 +0300

    fix container css

--- src/app/click-message-dialog/click-message-dialog-demo/click-message-dialog-demo.component.ts
+++ src/app/click-message-dialog/click-message-dialog-demo/click-message-dialog-demo.component.ts
@@ -19,24 +19,24 @@ export class ClickMeseageDialogDemoComponent implements OnInit {
     this.errorDialogData.title = 'Error';
     this.errorDialogData.okButtonString = 'Goy it';
     this.errorDialogData.cancelButtonString = 'Cancel';
-    this.errorDialogData.message =
-      "Analytics hasn't been set up yet. Please follow the setup instructions as described in the documentation.";
+    this.errorDialogData.message = `Analytics hasn't been set up yet.
+    Please follow the setup instructions as described in the documentation.`;
     this.errorDialogData.dialogType = DialogType.error;
     this.errorDialogData.buttonOptions = ButtonOptions.ok;
 
     this.infoDialogData.title = 'Notic';
     this.infoDialogData.okButtonString = 'Apply';
     this.infoDialogData.cancelButtonString = 'Cancel';
-    this.infoDialogData.message =
-      "Analytics hasn't been set up yet. Please follow the setup instructions as described in the documentation.";
+    this.infoDialogData.message = `Analytics hasn't been set up yet.
+    Please follow the setup instructions as described in the documentation.`;
     this.infoDialogData.dialogType = DialogType.info;
     this.infoDialogData.buttonOptions = ButtonOptions.okCancel;
 
     this.warningDialogData.title = 'Warning';
     this.warningDialogData.okButtonString = 'Apply';
     this.warningDialogData.cancelButtonString = 'Cancel';
-    this.warningDialogData.message =
-      "Analytics hasn't been set up yet. Please follow the setup instructions as described in the documentation.";
+    this.warningDialogData.message = `Analytics hasn't been set up yet.
+    Please follow the setup instructions as described in the documentation.`;
     this.warningDialogData.dialogType = DialogType.warning;
     this.warningDialogData.buttonOptions = ButtonOptions.okCancel;
   }
